7a00af1585c424e04568dfd011afcbe8609e14cb
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2021-09-30 19:27:23 +0700

    Added world 1-1 excerpt tilemap
    Added tilemap inside Textures folder along with tileset file (mario tileset) and its source

---
+++ Sprite/textures/Mario tileset.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Mario tileset" tilewidth="16" tileheight="16" tilecount="924" columns="33">
+ <image source="Mario tileset.png" trans="55aaff" width="528" height="448"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16.0054" height="15.9828"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16.0054" height="16.0054"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16.0054" height="16.0054"/>
+  </objectgroup>
+ </tile>
+</tileset>
